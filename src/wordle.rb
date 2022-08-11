f864acb74258b7a21bf4de3cf3d633560405d6aa
Chris Taylor <ctaylor534@gmail.com>
2022-08-11 08:55:56 -0700

    starts drawing of board colors, needs fine tuning

--- src/wordle.rb
+++ src/wordle.rb
@@ -11,6 +11,26 @@ class Wordle < Gosu::Window
 
   def draw
     @background.draw(0, 0, 0)
+    draw_board()
+  end
+
+  def draw_board
+    y = 0
+    @game.board.board.each do |row|
+      x = 0
+      row.each do |square|
+        if square == 1
+          c = Gosu::Color::YELLOW
+        elsif square == 2
+          c = Gosu::Color::GREEN
+        else
+          c = Gosu::Color::BLUE
+        end
+        draw_rect(290 + (42 * x), 77 + (47 * y), 31, 33, c, z = 1, mode = :default)
+        x += 1
+      end
+      y += 1
+    end
   end
 
   def update
