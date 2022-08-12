84b858e10b2846e0a297ee455a8c004996b7f6ed
Chris Taylor <ctaylor534@gmail.com>
2022-08-11 22:09:47 -0700

    Adds drawing of guesses and changing of colors for letters

--- src/wordle.rb
+++ src/wordle.rb
@@ -16,17 +16,71 @@ class Wordle < Gosu::Window
 
   def draw_board
     y = 0
+    y_add = 0
     @game.board.board.each do |row|
       x = 0
+      if y > 3
+        y_add = 2
+      elsif y > 1
+        y_add = 1
+      end
+      x_add = 0
       row.each do |square|
+        if x > 2
+          x_add = 2
+        elsif x > 0
+          x_add = 1
+        end
         if square == 1
           c = Gosu::Color::YELLOW
         elsif square == 2
           c = Gosu::Color::GREEN
         else
-          c = Gosu::Color::BLUE
+          c = Gosu::Color::WHITE
         end
-        draw_rect(290 + (42 * x), 77 + (47 * y), 31, 33, c, z = 1, mode = :default)
+        draw_rect(
+          290 + (42 * x) + x_add,
+          77 + (46 * y) + y_add,
+          32, 35, c, z = 1, mode = :default
+        )
+        x += 1
+      end
+      y += 1
+    end
+    x_add = 0
+    x = 0
+    y = @game.board.board_guesses.length
+    y_add = 0
+    if y > 3
+      y_add = 2
+    elsif y > 1
+      y_add = 1
+    end
+    @game.board.current_guess.each do |letter|
+      if x > 2
+        x_add = 2
+      elsif x > 0
+        x_add = 1
+      end
+      Gosu::Image.from_text(letter, 35, options = { :width => 32, :align => :center }).draw(290 + (42 * x) + x_add, 77 + (46 * y) + y_add, 2, scale_x = 1, scale_y = 1, color = Gosu::Color::BLACK)
+      x += 1
+    end
+    y = 0
+    @game.board.board_guesses.each do |row|
+      x = 0
+      if y > 3
+        y_add = 2
+      elsif y > 1
+        y_add = 1
+      end
+      x_add = 0
+      row.each do |square|
+        if x > 2
+          x_add = 2
+        elsif x > 0
+          x_add = 1
+        end
+        Gosu::Image.from_text(square, 35, options = { :width => 32, :align => :center }).draw(290 + (42 * x) + x_add, 77 + (46 * y) + y_add, 2, scale_x = 1, scale_y = 1, color = Gosu::Color::BLACK)
         x += 1
       end
       y += 1
